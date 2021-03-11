d97e4304703be7f44a946e51b248ba494ec417b4
bleak.twig <bruno.benkel@gmail.com>
2021-03-11 13:43:59 -0300

    Externalized event handling

---
+++ src/event_handler.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Gruid Translator by Bruno Benkel
+# To the extent possible under law, the person who associated CC0 with Gruid Translator has waived
+# all copyright and related or neighboring rights to Gruid Translator.
+
+"""
+Handles GEMC events. Currently only extracts the relevant hits for an event.
+"""
+
+import constants as c
+
+def extract_hits(event):
+    """
+    Extract hits from an event.
+    :param event: one event in the format defined by the store_event() method.
+    :return:      a 2-tuple containing dictionaries describing the hits in arrays. Dictionaries' keys are the
+                  following:
+                    * n: hit identifier. Unused by this program, but useful in reconstruction.
+                    * x: x position of the hit in cm.
+                    * y: y position of the hit in cm.
+                    * t: time of the hit in ns (t=0 is defined as the moment when the event takes place).
+                    * E: energy deposited by the hit.
+    """
+    if event is None: return None
+    # Define hits dictionaries (one per detecting surface).
+    hits = ({'n' : [], 'x' : [], 'y' : [], 't' : [], 'E' : [],},
+            {'n' : [], 'x' : [], 'y' : [], 't' : [], 'E' : [],},)
+
+    for hi in range(len(event[c.IDBANK][c.S_HITN])):
+        if event[c.IRBANK]["pid"]    [hi] != '0': continue # Ignore hits that don't come from photons.
+        if event[c.IRBANK]["totEdep"][hi] == '0': continue # Ignore hits with no energy deposited.
+        vol = -1 # Save the volume hit.
+        volid = int(int(event[c.IDBANK][c.S_VOL][hi])/10**8)
+        if volid == c.SENSOR1A_ID or volid == c.SENSOR1B_ID: vol = 0
+        if volid == c.SENSOR2A_ID or volid == c.SENSOR2B_ID: vol = 1
+        if vol == -1: continue
+
+        # Add hit to dictionary.
+        hits[vol]['n'].append(float(event[c.IDBANK][c.S_HITN][hi]))  # hit identifier.
+        hits[vol]['x'].append(float(event[c.IRBANK][c.S_X][hi])/10.) # x position (in cm).
+        hits[vol]['y'].append(float(event[c.IRBANK][c.S_Y][hi])/10.) # y position (in cm).
+        hits[vol]['t'].append(float(event[c.IRBANK][c.S_T][hi]))     # Time.
+        hits[vol]['E'].append(float(event[c.IRBANK][c.S_E][hi]))     # Energy deposited.
+
+    return hits
--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ from scipy.sparse import csr_matrix
 
 import constants as c
 import file_io as io
+import event_handler as ehandler
 
 # FLAGS
 # --filename or -f: Filename to open. No default value.
@@ -28,48 +29,13 @@ import file_io as io
 NROWS = 11
 NCOLS = 11
 
-(metadata, events) = io.load_file("/home/twig/data/code/babycal/bcal_generator/output_test_1.txt")
-
 import pprint
 pp = pprint.PrettyPrinter(indent=4, width=100, compact=True)
 
-pp.pprint(metadata)
-
-# Extract hits from an event.
-def extract_hits(event):
-    """
-    Extract hits from an event.
-    :param event: one event in the format defined by the store_event() method.
-    :return:      a 2-tuple containing dictionaries describing the hits in arrays. Dictionaries' keys are the
-                  following:
-                    * n: hit identifier. Unused by this program, but useful in reconstruction.
-                    * x: x position of the hit in cm.
-                    * y: y position of the hit in cm.
-                    * t: time of the hit in ns (t=0 is defined as the moment when the event takes place).
-                    * E: energy deposited by the hit.
-    """
-    if event == None: return None
-    # Define hits dictionaries (one per detecting surface).
-    hits = ({'n' : [], 'x' : [], 'y' : [], 't' : [], 'E' : [],},
-            {'n' : [], 'x' : [], 'y' : [], 't' : [], 'E' : [],},)
-
-    for hi in range(len(event[c.IDBANK][c.S_HITN])):
-        if event[c.IRBANK]["pid"]    [hi] != '0': continue # Ignore hits that don't come from photons.
-        if event[c.IRBANK]["totEdep"][hi] == '0': continue # Ignore hits with no energy deposited.
-        vol = -1 # Save the volume hit.
-        volid = int(int(event[c.IDBANK][c.S_VOL][hi])/10**8)
-        if volid == c.SENSOR1A_ID or volid == c.SENSOR1B_ID: vol = 0
-        if volid == c.SENSOR2A_ID or volid == c.SENSOR2B_ID: vol = 1
-        if vol == -1: continue
-
-        # Add hit to dictionary.
-        hits[vol]['n'].append(float(event[c.IDBANK][c.S_HITN][hi]))  # hit identifier.
-        hits[vol]['x'].append(float(event[c.IRBANK][c.S_X][hi])/10.) # x position (in cm).
-        hits[vol]['y'].append(float(event[c.IRBANK][c.S_Y][hi])/10.) # y position (in cm).
-        hits[vol]['t'].append(float(event[c.IRBANK][c.S_T][hi]))     # Time.
-        hits[vol]['E'].append(float(event[c.IRBANK][c.S_E][hi]))     # Energy deposited.
-
-    return hits
+(metadata, events) = io.load_file("/home/twig/data/code/babycal/bcal_generator/output_test_1.txt")
+for event in events:
+    hits = ehandler.extract_hits(event)
+    pp.pprint(hits)
 
 def generate_timeseries(hits, dt, dx, dy):
     """
